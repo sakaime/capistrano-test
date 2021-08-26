a5f9c3e6370a37724a0aa42409d06586f1bf9825
sakaime <sakaime2@gmail.com>
2021-08-26 14:17:17 +0900

    modify typo

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,7 +1,7 @@
 server "54.95.44.201", user: "deploy", roles: %w{web db app}
 
 set :ssh_options, {
-  user: "deploy"
+  user: "deploy",
   port: 22,
   keys: %w(~/Downloads/capistrano-test-key.pem),
   forward_agent: true,
